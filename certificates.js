6b7c3eec8d18376433e9e99a62fc8882c4e35ee0
{Bhanu} <{sangulabhanuteja@gmail.com}>
2026-03-16 11:19:20 +0530

    Initial portfolio commit

---
+++ certificates.js
@@ -0,0 +1,33 @@
+const certificatesData = {
+  "certificates": [
+    {
+      "name": "Legacy Responsive Web Design V8 Certification",
+      "image": "vector-colorful-logo-design_1002026-45.avif",
+      "file": "https://www.freecodecamp.org/certification/bhanu2605/responsive-web-design",
+      "category": "web development",
+      "issuer": "freeCodeCamp"
+    },
+    {
+      "name": "Build Generative AI Apps and Solutions with No- Code Tools",
+      "image": "1-b92f1acd-817c-4244-a8c9-438ca64fed90 (1).pdf",
+      "file": "https://infyspringboard.onwingspan.com/public-assets/infosysheadstart/cert/lex_auth_0135015511562403847605/1-b92f1acd-817c-4244-a8c9-438ca64fed90.pdf",
+      "category": "artificial intelligence",
+      "issuer": "Udemy"
+    },
+    {
+      "name": "Computational Theory: Language Principle & Finite Automata Theory",
+      "image": "1-b92f1acd-817c-4244-a8c9-438ca64fed90 (1).pdf",
+      "file": "https://infyspringboard.onwingspan.com/public-assets/infosysheadstart/cert/lex_auth_0135015511562403847605/1-b92f1acd-817c-4244-a8c9-438ca64fed90.pdf",
+      "category": "computer science",
+      "issuer": "Infosys"
+    },
+    {
+      "name": "Object Oriented Programming Using C++",
+      "image": "12301569_836_20_08_2025.pdf",
+      "file": "https://files.lpu.in/umsweb/skilldevcourse/SkillDevelopmentCertificates/12301569_836_20_08_2025.pdf?_gl=1*lmip3l*_gcl_au*MTA4MzgxODIyOS4xNzY2Mzc1NzY1",
+      "category": "programming",
+      "issuer": "Infosys"
+    }
+  ],
+  "folders": {}
+};
